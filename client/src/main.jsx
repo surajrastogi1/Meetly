b5664e4f63b4ef702950842eeca97f179274f417
surajrastogi1 <surajrastogi606@gmail.com>
2026-09-01 00:40:44 +0530

    Login and Register UI secured by clerk

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { ClerkProvider } from '@clerk/react'
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if(!PUBLISHABLE_KEY){
+  throw new Error("Missing VITE_CLERK_PUBLISHABLE_KEY in client environment")
+}
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/login'>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ClerkProvider>,
 )
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,25 @@
+import { SignIn, SignUp } from '@clerk/react';
+import { useUser } from '@clerk/react-router';
 import React from 'react'
+import { Navigate } from 'react-router-dom';
 
-const Login = () => {
+const Login = ({mode="login"}) => {
+
+    const isRegister = mode === 'register';
+    const {isLoaded, isSignedIn} = useUser();
+    
+    if(isLoaded && isSignedIn){
+        <Navigate path="/dashboard" replace/>
+    }
+
   return (
-    <div>Login</div>
+    <div className="min-h-screen w-full bg-[url('/login_bg.png')] text-slate-800 p-4 md:p-6 lg:p-8 flex items-center justify-center font-sans">
+        {isRegister ? (
+            <SignUp routing='path' path='/register' signInUrl='/login' fallbackRedirectUrl='/dashboard' />
+        ) : (
+            <SignIn routing='path' path='/login' signUpUrl='/register' fallbackRedirectUrl='/dashboard'/>
+        )}
+    </div>
   )
 }
 
