ae845db41da6f2cd7b182a0c139dcac4db9cf092
surajrastogi1 <surajrastogi606@gmail.com>
2026-09-03 14:06:24 +0530

    Created navbar , navbar links and footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='w-full max-w-305 mx-auto bg-white/10 backdrop-blur xl:rounded-t-xl px-6 py-4 text-center'>
+      <p className='text-xs font-medium text-white'>© 2026 Meetly. All rights reserved</p>
+    </footer>
   )
 }
 
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,81 @@
 import React from 'react'
-
+import { Link, useLocation } from 'react-router-dom'
+import { dummyUser } from '../assets/asset'
+import { AstroidIcon, HistoryIcon, LayoutDashboardIcon } from 'lucide-react';
+import { UserButton } from '@clerk/react-router'
 const Navbar = () => {
+
+  const {user, isSignedIn} = {user : dummyUser, isSignedIn: true}
+  const location = useLocation()
+  const userName = user?.fullName || user?.firstName || user?.primaryEmailAddress?.emailAddress?.split("@")[0] || "User"
+
   return (
-    <div>Navbar</div>
+    <header className='w-full max-w-305 mx-auto bg-white/90 backdrop-blur xl:rounded-b-xl sticky top-0 z-40 px-6 py-4 flex items-center justify-between border border-slate-200 '>
+      {/* Brand Logo and navigation links */}
+      <div className='flex items-center gap-6'>
+
+
+        <Link to="/dashboard" className='flex items-center gap-1.5'>
+        <img src="logo.svg" alt="logo" className='size-6.5'/>
+        <span className='text-2xl font-medium tracking-tight text-slate-900 flex items-center'>
+            Meetly<span className='text-primary'>.</span>
+        </span>  
+        </Link>
+
+        {isSignedIn && (
+          <nav className='hidden md:flex items-center gap-1.5 ml-2'>
+            <Link to="/dashboard"
+            className={`px-3.5 py-1.5 rounded-full text-xs font-medium transition-all flex items-center gap-1.5 
+            ${location.pathname === "/dashboard" 
+            ? "ring ring-blue-100 bg-blue-50 text-slate-800" 
+            : "text-slate-500 hover:text-slate-900 hover:bg-slate-50"}`}>
+
+              <LayoutDashboardIcon className='w-3.5 h-3.5' />
+              Dashboard
+              
+
+            </Link>
+            <Link to="/session"
+            className={`px-3.5 py-1.5 rounded-full text-xs font-medium transition-all flex items-center gap-1.5 
+            ${location.pathname === "/session" 
+            ? "ring ring-blue-100 bg-blue-50 text-slate-800" 
+            : "text-slate-500 hover:text-slate-900 hover:bg-slate-50"}`}>
+
+              <HistoryIcon className='w-3.5 h-3.5' />
+              Session
+              
+
+            </Link>
+            <Link to="/pricing"
+            className={`px-3.5 py-1.5 rounded-full text-xs font-medium transition-all flex items-center gap-1.5 
+            ${location.pathname === "/pricing" 
+            ? "ring ring-blue-100 bg-blue-50 text-slate-800" 
+            : "text-slate-500 hover:text-slate-900 hover:bg-slate-50"}`}>
+
+              <AstroidIcon className='w-3.5 h-3.5' />
+              Pricing
+              
+
+            </Link>
+          </nav>
+        )}
+
+      </div>
+
+      {/* Right Profile /  userButton  */}
+      <div className='flex items-center gap-4'>
+        <Link to="/session" className='md:hidden text-xs font-medium text-slate-600 hover:text-primary flex items-center gap-1'>
+          <HistoryIcon className='w-3.5 h-3.5'/>
+          Session
+        </Link>
+        <span className='font-medium hidden sm:inline tracking-wide text-sm text-slate-700'>
+          Welcome, {userName}
+        </span>
+        <UserButton afterSignOutUrl = "/login" />
+        
+      </div>
+
+    </header>
   )
 }
 
